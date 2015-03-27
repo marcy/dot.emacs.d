2c0dcf6fb5d2ec8aac4a6b945acfccc206a1a25d
oyamada_masashi <masashi.oyamada@gmail.com>
2015-03-27 18:43:44 +0900

    change

--- inits/33-prog.el
+++ inits/33-prog.el
@@ -140,3 +140,40 @@
 (require 'rspec-mode)
 (eval-after-load 'rspec-mode
   '(rspec-install-snippets))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; rubocop
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'rubocop)
+(add-hook 'ruby-mode-hook 'rubocop-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flycheck-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+(require 'flycheck)
+(add-hook 'ruby-mode-hook 'flycheck-mode)
+(flycheck-define-checker ruby-rubocop
+  "A Ruby syntax and style checker using the RuboCop tool.
+
+See URL `http://batsov.com/rubocop/'."
+  :command ("rubocop" "--format" "emacs" "--silent"
+            (config-file "--config" flycheck-rubocoprc)
+            source)
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+            line-end)
+   (error line-start
+          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+          line-end))
+  :modes (enh-ruby-mode motion-mode))
+
+;;; ==================================================================
+;;;                go
+;;; ==================================================================
+(require 'go-mode)
+(add-hook 'go-mode-hook
+      '(lambda ()
+         (setq tab-width 2)
+         ))
