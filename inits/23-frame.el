dfacbeb90f994ee298173710bb8740f692b17c5f
oyamada_masashi <masashi.oyamada@gmail.com>
2015-07-22 15:43:05 +0900

    font

--- inits/23-frame.el
+++ inits/23-frame.el
@@ -38,37 +38,22 @@
 ;;; ============================================================
 ;; Fonts
 (when (eq window-system 'ns)
-  (let ((my-font-height 120)
-        (my-font (cond
-                  (nil "Monaco")  ;; XCode 3.1 で使っているフォント
-                  (t "Menlo")     ;; XCode 3.2 で使ってるフォント
-                  ))
-        (my-font-ja "Hiragino Maru Gothic Pro"))
-    (setq mac-allow-anti-aliasing t)
-
-    ;; フォントサイズの微調節 (12ptで合うように)
-    (setq face-font-rescale-alist
-          '(("^-apple-hiragino.*" . 1.2)
-            (".*osaka-bold.*" . 1.2)
-            (".*osaka-medium.*" . 1.2)
-            (".*courier-bold-.*-mac-roman" . 1.0)
-            (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-            (".*monaco-bold-.*-mac-roman" . 0.9)
-            ("-cdac$" . 1.3)))
-
-    ;; デフォルトフォント設定
-    (when my-font
-      (set-face-attribute 'default nil :family my-font :height my-font-height)
-      ;;(set-frame-font (format "%s-%d" my-font (/ my-font-height 10)))
-      )
-
-    ;; 日本語文字に別のフォントを指定
-    (when my-font-ja
-      (let ((fn (frame-parameter nil 'font))
-            (rg "iso10646-1"))
-        (set-fontset-font fn 'katakana-jisx0201 `(,my-font-ja . ,rg))
-        (set-fontset-font fn 'japanese-jisx0208 `(,my-font-ja . ,rg))
-        (set-fontset-font fn 'japanese-jisx0212 `(,my-font-ja . ,rg))))))
+  (create-fontset-from-ascii-font
+   "Menlo-12:weight=normal:slant=normal"
+   nil
+   "menlokakugo")
+  (set-fontset-font
+   "fontset-menlokakugo"
+   'unicode
+   (font-spec :family "Hiragino Kaku Gothic ProN" :size 12)
+   nil
+   'append)
+  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+  (add-to-list 'initial-frame-alist '(font . "fontset-menlokakugo"))
+  (add-to-list 'face-font-rescale-alist
+               '(".*Hiragino Kaku Gothic ProN.*" . 1.2))
+  (add-hook 'after-init-hook
+            (lambda () (set-frame-font "fontset-menlokakugo"))))
 
 ;;; ============================================================
 ;;;             frame
