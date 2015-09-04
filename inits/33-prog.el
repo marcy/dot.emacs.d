244807148dc3b60cffa644a8bd0d4ef4c97ead72
marcy <masashi.oyamada@gmail.com>
2015-09-04 10:12:56 +0900

    web-mode

--- inits/33-prog.el
+++ inits/33-prog.el
@@ -22,6 +22,21 @@
           '(lambda() (scss-custom)))
 
 ;;; ==================================================================
+;;;             web-mode
+;;; ==================================================================
+(require 'web-mode)
+;; 拡張子の設定
+(add-to-list 'auto-mode-alist '("\\.eex$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$"     . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?$"   . web-mode))
+;;; インデント数
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;;; ==================================================================
 ;;;             markdown
 ;;; ==================================================================
 (setq auto-mode-alist (append
