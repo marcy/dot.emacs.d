060b042fa77cd7ba579035a7805f1ece834264cc
marcy <masashi.oyamada@gmail.com>
2016-10-12 15:00:50 +0900

    go-mode

--- inits/33-go.el
+++ inits/33-go.el
@@ -1,5 +1,12 @@
 (require 'go-mode)
+(require 'company)
+(require 'company-go)
 
-(add-hook 'go-mode-hook
-          '(lambda ()
-             (setq tab-width 2)))
+(add-hook 'go-mode-hook 'company-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
+(add-hook 'go-mode-hook (lambda()
+                          (add-hook 'before-save-hook 'gofmt-before-save)
+                          (local-set-key (kbd "M-.") 'godef-jump)
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)
+                          (setq tab-width 2)))
