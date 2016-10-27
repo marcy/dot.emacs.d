295c341140c019aaf34a23c96910e38a498ab227
marcy <masashi.oyamada@gmail.com>
2016-10-27 13:11:24 +0900

    R.I.P.

--- inits/00-init.el
+++ inits/00-init.el
@@ -11,7 +11,6 @@
 (set-keyboard-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
-;(set-clipboard-coding-system 'utf-8)
 (set-file-name-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
--- inits/43-tags.el
+++
@@ -1,2 +0,0 @@
-(setq tags-table-list
-      '("~"))
