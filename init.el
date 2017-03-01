ecafa53e3c735ab4de1f0e92fea2b2a7ac6a1e9a
marcy <masashi.oyamada@gmail.com>
2017-03-01 18:58:49 +0900

    custom.el

--- init.el
+++ init.el
@@ -17,22 +17,6 @@
 
 ;; init loader
 (require 'init-loader)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ag-highlight-search t)
- '(ag-reuse-buffers (quote nil))
- '(ag-reuse-window (quote nil))
- '(anzu-deactivate-region t)
- '(anzu-mode-lighter "")
- '(anzu-search-threshold 1000)
- '(custom-safe-themes
-   (quote
-    ("5d3e0746023fc5e246eb3e0e48c1ccb5ce0387fc4273896c6cf02ee349c2eba8" default)))
- '(init-loader-show-log-after-init (quote error-only))
- '(send-mail-function (quote smtpmail-send-it)))
 (init-loader-load "~/.emacs.d/inits")
 
 ;; after init
@@ -42,18 +26,5 @@
     (message "init time: %.3f sec"
              (float-time (time-subtract after-init-time before-init-time)))))
 (put 'upcase-region 'disabled nil)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(markdown-header-delimiter-face ((t (:inherit org-mode-line-clock))))
- '(markdown-header-face-1 ((t (:inherit outline-1 :weight bold))))
- '(markdown-header-face-2 ((t (:inherit outline-2 :weight bold))))
- '(markdown-header-face-3 ((t (:inherit outline-3 :weight bold))))
- '(markdown-header-face-4 ((t (:inherit outline-4 :weight bold))))
- '(markdown-header-face-5 ((t (:inherit outline-5 :weight bold))))
- '(markdown-header-face-6 ((t (:inherit outline-6 :weight bold))))
- '(markdown-pre-face ((t (:inherit org-formula)))))
 (put 'downcase-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
